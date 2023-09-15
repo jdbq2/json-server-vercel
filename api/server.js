6905b09899601874115dd913e9db9229695a9a05
Juan Diego Bohorquez Quiroga <jdbq2@hotmail.com>
2023-09-15 12:14:09 -0500

    Tiempo de delay actualizado

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-    setTimeout(next, 3000);
+    setTimeout(next, 1000);
 });
 
 /* server.use(
