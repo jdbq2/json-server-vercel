24eba2727d55e77f4c9549255a9c21d08d764976
Juan Diego Bohorquez Quiroga <jdbq2@hotmail.com>
2023-09-15 08:11:14 -0500

    Server Setup

---
+++ api/server.js
@@ -0,0 +1,44 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+    setTimeout(next, 3000);
+});
+// Add this before server.use(router)
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+        "/blog/:resource/:id/show": "/:resource/:id",
+    })
+);
+server.post("/api/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const users = await router.db.get("users").value();
+        const user = users.find(
+            (u) => u.email === email && u.password === password
+        );
+        if (user) {
+            res.status(200).json({ message: "Inicio de sesión exitoso", user });
+        } else {
+            res.status(401).json({ error: "Credenciales incorrectas" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "Error interno en el servidor",
+        });
+    }
+});
+server.use(router);
+server.listen(3000, () => {
+    console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
