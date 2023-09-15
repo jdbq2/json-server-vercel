986d096c6b139b8a51dcda5c9da1d487aa3be84e
Juan Diego Bohorquez Quiroga <jdbq2@hotmail.com>
2023-09-15 08:27:17 -0500

    Changes made

--- api/server.js
+++ api/server.js
@@ -9,13 +9,14 @@ server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
     setTimeout(next, 3000);
 });
-// Add this before server.use(router)
-server.use(
+
+/* server.use(
     jsonServer.rewriter({
         "/api/*": "/$1",
         "/blog/:resource/:id/show": "/:resource/:id",
     })
-);
+); */
+
 server.post("/api/login", async (req, res) => {
     try {
         const { email, password } = req.body;
